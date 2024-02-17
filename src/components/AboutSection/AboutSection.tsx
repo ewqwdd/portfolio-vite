5f49c2b644f7399f8e1a785bef734e40a31305b6
Mykolai Skydan <skmykolai@gmail.com>
2024-02-17 22:45:03 +0000

    Update AboutSection.tsx

--- src/components/AboutSection/AboutSection.tsx
+++ src/components/AboutSection/AboutSection.tsx
@@ -1,9 +1,8 @@
-
 interface AboutSectionProps {
-    margin?: number
+  margin?: number;
 }
 
-export default function AboutSection({margin}: AboutSectionProps) {
+export default function AboutSection({ margin }: AboutSectionProps) {
   return (
     <section
       id="About"
@@ -12,17 +11,14 @@ export default function AboutSection({margin}: AboutSectionProps) {
         marginLeft: margin,
       }}
     >
-        <h2 className="text-h2Clamp font-bold">
-            About me:
-        </h2>
+      <h2 className="text-h2Clamp font-bold">About me:</h2>
       <p className="text-p20Clamp max-[520px]:text-p18Clamp leading-snug font-extralight mt-2 lg:mt-4">
-        I started learning programming with C++ in 2022, then explored Python.
-        In the summer of 2023, I got into web development with Javascript. I
-        {"'"}ve spent the last six months improving my skills and working on
-        different projects. Recently, I{"'"}ve been freelancing for two months.
-        Though I haven{"'"}t worked on commercial projects in a team, I{"'"}m
-        eager for new challenges and believe I can contribute effectively to
-        your team.
+        Looking for a talented developer to bring your ideas to life? I{"'"}m your
+        person. I specialize in Frontend development, software development,
+        optimization, cross-browser web applications, and open APIs integration.
+        From analyzing tasks to delivering a simple, usable end product, I{"'"}ve
+        got the developing cycle covered. Let{"'"}s work together to turn your ideas
+        into reality.
       </p>
     </section>
   );
