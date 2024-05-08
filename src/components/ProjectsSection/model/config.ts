b05b2bee3a092ccc0ce506619522f658efef4fc6
Mykolai Skydan <skmykolai@gmail.com>
2024-05-08 19:13:44 +0100

    add google sheets backend

--- src/components/ProjectsSection/model/config.ts
+++ src/components/ProjectsSection/model/config.ts
@@ -1,47 +1,49 @@
-import { ReactNode } from "react"
+import axios from "axios";
+import { ReactNode } from "react";
 
 export interface Project {
-    title: string
-    tags: string[]
-    image: string
-    description?: ReactNode | string
-    href?: string
+  title: string;
+  tags: string[];
+  image: string;
+  description?: ReactNode | string;
+  href?: string;
 }
 
-export const projects: Project[] = [
-    {
-        image: '/projects/chess-puzzles.png',
-        tags: [
-            'React',
-            'Redux',
-            'Typescript',
-            'Nest.js',
-            'DnD'
-        ],
-        description: "I built this project with React and Nest.js, learning new things along the way. It was challenging, especially since it was my first time using Nest. The trickiest part was creating a chessboard and making drag-and-drop smooth for users. I also tried to optimize the project as much as possible with async components. Overall, it was good practice.",
-        title: 'Chess Puzzles',
-        href: 'https://tubular-kangaroo-cedb4e.netlify.app/'
-    },
-    {
-        image: '/projects/cloudstorage.png',
-        tags: [
-            'JWT',
-            'Nest.js',
-            'Typescript',
-            'Next.js'
-        ],
-        description: 'This app aimed to add JWT authorization with Next.js and Nest.js. The trickiest part was creating a selection box, without using any external libraries.',
-        title: 'SUPERCLOUD',
-        href: 'https://cloudstorage-frontend-eosin.vercel.app/'
-    },
-    {
-        image: '/projects/metamorphix.png',
-        tags: [
-            'Next.js',
-            'Typescript',
-        ],
-        description: "I worked on a freelance project – a website for a Discord server. I chose Next.js since there wasn't a need for a complicated backend. Looking back, I see I made some less-than-perfect decisions, but it was a learning experience. I've taken those lessons to heart and now avoid those mistakes in my current work.",
-        title: 'METAMORPHIX',
-        href: 'https://www.metamorphix.me/'
-    }
-]
+export type ProjectFetched = [string, string, string, string, string, string, string]
+
+const url =
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vTJVcsHQjLmG2UpN-1jm7h0z4I2kWxJribALXcYkPW8y9a1Cq2BaMoaIaHxSE4O05r3CNHpJ3i_YWqt/pub?output=csv";
+
+export const fetchData = async (): Promise<ProjectFetched[]> => {
+  const data = (await axios.get(url)).data as string;
+  const rows = data.split(/\r?\n/);
+  const splitted = rows.map((elem) => elem.split(",")) as ProjectFetched[];
+  return splitted
+};
+
+export const projects = [
+  {
+    image: "/projects/chess-puzzles.png",
+    tags: ["React", "Redux", "Typescript", "Nest.js", "DnD"],
+    description:
+      "I built this project with React and Nest.js, learning new things along the way. It was challenging, especially since it was my first time using Nest. The trickiest part was creating a chessboard and making drag-and-drop smooth for users. I also tried to optimize the project as much as possible with async components. Overall, it was good practice.",
+    title: "Chess Puzzles",
+    href: "https://tubular-kangaroo-cedb4e.netlify.app/",
+  },
+  {
+    image: "/projects/cloudstorage.png",
+    tags: ["JWT", "Nest.js", "Typescript", "Next.js"],
+    description:
+      "This app aimed to add JWT authorization with Next.js and Nest.js. The trickiest part was creating a selection box, without using any external libraries.",
+    title: "SUPERCLOUD",
+    href: "https://cloudstorage-frontend-eosin.vercel.app/",
+  },
+  {
+    image: "/projects/metamorphix.png",
+    tags: ["Next.js", "Typescript"],
+    description:
+      "I worked on a freelance project – a website for a Discord server. I chose Next.js since there wasn't a need for a complicated backend. Looking back, I see I made some less-than-perfect decisions, but it was a learning experience. I've taken those lessons to heart and now avoid those mistakes in my current work.",
+    title: "METAMORPHIX",
+    href: "https://www.metamorphix.me/",
+  },
+];
--- src/components/ProjectsSection/ui/ProjectSection.tsx
+++ src/components/ProjectsSection/ui/ProjectSection.tsx
@@ -1,14 +1,16 @@
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { useScreen } from "../../../lib/hooks/useScreen";
 import Spinner from "../../Spinner/Spinner";
-import { projects } from "../model/config";
+import { Project, ProjectFetched, fetchData } from "../model/config";
 import ProjectMobileAsync from "./ProjectMobile/ProjectMobile.async";
 import ProjectMobileLoader from "./ProjectMobile/ProjectMobileLoader";
 import ProjectAsync from "./Project/Project.async";
 import ProjectLoader from "./Project/ProjectLoader";
 import ProjectSmallMobileAsync from "./ProjectSmallMobile/ProjectSmallMobile.asynct";
 import ProjectSmallLoader from "./ProjectSmallMobile/ProjectSmallLoader";
-import './Project.css'
+import "./Project.css";
+
+const perPage = 3;
 
 interface ProjectSectionProps {
   countWidth?: boolean;
@@ -16,32 +18,47 @@ interface ProjectSectionProps {
   mobile?: boolean;
 }
 
-export default function ProjectSection({
-  countWidth,
-  mobile,
-}: ProjectSectionProps) {
+export default function ProjectSection({ countWidth, mobile }: ProjectSectionProps) {
   const { width } = useScreen();
-  let Cmp: typeof ProjectMobileAsync
-  let Loader: typeof ProjectMobileLoader
+  const [projects, setProjects] = useState<ProjectFetched[]>([]);
+  const [page, setPage] = useState<number>(1);
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    setError(null);
+    setIsLoading(true);
+    fetchData()
+      .then((data) => {
+        setProjects(data);
+        setIsLoading(false);
+        console.log(data);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsLoading(false);
+      });
+  }, []);
+
+  let Cmp: typeof ProjectMobileAsync;
+  let Loader: typeof ProjectMobileLoader;
   if (!width) {
-    return (
-      <Spinner className="w-48 h-48v absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2" />
-    );
+    return <Spinner className="w-48 h-48v absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2" />;
   }
   if (mobile) {
     if (width <= 460) {
-      Cmp = ProjectSmallMobileAsync
-      Loader = ProjectSmallLoader
-    }
-    else {
-      Cmp = ProjectMobileAsync
-      Loader = ProjectMobileLoader
+      Cmp = ProjectSmallMobileAsync;
+      Loader = ProjectSmallLoader;
+    } else {
+      Cmp = ProjectMobileAsync;
+      Loader = ProjectMobileLoader;
     }
   } else {
-    Cmp = ProjectAsync
-    Loader = ProjectLoader
+    Cmp = ProjectAsync;
+    Loader = ProjectLoader;
   }
   const margin = width >= 1310 ? (width - 1280) / 2 : 12;
+
   return (
     <section
       id="Projects"
@@ -50,15 +67,43 @@ export default function ProjectSection({
         marginLeft: countWidth ? margin : 0,
       }}
     >
-      <h2 className="text-h2Clamp font-bold">Projects:</h2>
-      <div className="group flex flex-col gap-2 scrollCustom">
-      <>
-          {projects.map((elem, index) => (
+      <div className="flex justify-between">
+        <h2 className="text-h2Clamp font-bold">Projects:</h2>
+        <div className="flex gap-2">
+          {new Array(Math.ceil(projects.length / perPage)).fill(0).map((_, index) => (
+            <button
+              className="size-8 bg-zinc-400/60 rounded-lg text-white transition-all opacity-60 hover:opacity-90"
+              disabled={index + 1 === page}
+              onClick={() => setPage(index + 1)}
+              style={{
+                opacity: index + 1 === page ? "1" : undefined,
+              }}
+            >
+              {index + 1}
+            </button>
+          ))}
+        </div>
+      </div>
+      <div className="group flex flex-col gap-2 scrollCustom relative transition-all">
+        <>
+          {isLoading && !projects
+            ? new Array(perPage).fill(0).map((_, index) => <Loader key={index} />)
+            : null}
+          {error && (
+            <span className="text-red-500 font-semibold text-4xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+              {error}
+            </span>
+          )}
+          {projects.slice((page - 1) * perPage, page * perPage).map((elem, index) => (
             <Suspense key={index} fallback={<Loader />}>
-            <Cmp
-              {...elem}
-              className="group-hover:opacity-70 transition-all"
-            />
+              <Cmp
+                title={elem[0]}
+                description={elem[1].replace(";", ",")}
+                tags={elem[2].split(";")}
+                href={elem[3]}
+                image={elem[4]}
+                className="group-hover:opacity-70 transition-all"
+              />
             </Suspense>
           ))}
         </>
